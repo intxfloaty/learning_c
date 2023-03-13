1e87e28175c0b0e8a0d17ca9e655f967eab20afe
Pravesh <praveshjha5@gmail.com>
2023-03-13 12:03:41 +0530

    fahr-celsius table

--- helloWorld..c
+++ helloWorld..c
@@ -1,13 +1,5 @@
 #include <stdio.h>
 
-int main()
-{
-    int a=1,b=1;
-    for (int i = 0; i < 10; ++i) {
-        // printf("%d %d ", a, b);
-        a+=b;
-        b+=a;
-    }
-    printf("%d %d\n", a, b);
-    printf("hello world\n");
+int main() {
+    printf("Hello, World\n");
 }
